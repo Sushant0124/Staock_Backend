771eda331f12479786e5ce848bc7e04b9b745fea
SUSHANT BANSAL <142045977+Sushant0124@users.noreply.github.com>
2024-09-09 16:19:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const url = process.env.MONGO_URL;
 
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["https://stock-frontend-murex.vercel.app/", "https://stock-dashboard-nine.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
