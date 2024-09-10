eb1c3b5b76d043e38477e5520eafb37ae1625eef
SUSHANT BANSAL <142045977+Sushant0124@users.noreply.github.com>
2024-09-10 18:24:47 +0530

    Update AuthController.js

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -17,6 +17,9 @@ module.exports.Signup = async (req, res, next) => {
     res.cookie("token", token, {
       withCredentials: true,
       httpOnly: true,
+      sameSite: 'None',
+  secure: true, 
+      path: '/'
     });
 
     res.status(201).json({
@@ -55,6 +58,9 @@ module.exports.Login = async (req, res, next) => {
        res.cookie("token", token, {
          withCredentials: true,
          httpOnly: true,
+         sameSite: 'None',
+  secure: true, 
+         path: '/'
        });
        res.status(201).json({ message: "User logged in successfully", success: true });
        next()
